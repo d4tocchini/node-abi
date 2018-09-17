a127084d1cbcaa5ab6ec9ca403a32c4167993a9f
D4 <d4@thegrid.io>
2018-09-17 04:50:54 -0700

    temporarily removed all but latest electron beta target

--- index.js
+++ index.js
@@ -49,49 +49,51 @@ function getTarget (abi, runtime) {
 }
 
 var supportedTargets = [
-  {runtime: 'node', target: '5.0.0', abi: '47', lts: false},
-  {runtime: 'node', target: '6.0.0', abi: '48', lts: false},
-  {runtime: 'node', target: '7.0.0', abi: '51', lts: false},
-  {runtime: 'node', target: '8.0.0', abi: '57', lts: new Date() < new Date(2019, 4, 31)},
-  {runtime: 'node', target: '9.0.0', abi: '59', lts: false},
-  {runtime: 'node', target: '10.0.0', abi: '64', lts: new Date(2018, 10, 1) < new Date() < new Date(2020, 4, 31)},
-  {runtime: 'electron', target: '0.36.0', abi: '47', lts: false},
-  {runtime: 'electron', target: '1.1.0', abi: '48', lts: false},
-  {runtime: 'electron', target: '1.3.0', abi: '49', lts: false},
-  {runtime: 'electron', target: '1.4.0', abi: '50', lts: false},
-  {runtime: 'electron', target: '1.5.0', abi: '51', lts: false},
-  {runtime: 'electron', target: '1.6.0', abi: '53', lts: false},
-  {runtime: 'electron', target: '1.7.0', abi: '54', lts: false},
-  {runtime: 'electron', target: '1.8.0', abi: '57', lts: false},
-  {runtime: 'electron', target: '2.0.0', abi: '57', lts: false},
-  {runtime: 'electron', target: '3.0.0-beta.1', abi: '64', lts: false},
-  {runtime: 'electron', target: '3.0.0-beta.10', abi: '64', lts: false}
+  // {runtime: 'node', target: '5.0.0', abi: '47', lts: false},
+  // {runtime: 'node', target: '6.0.0', abi: '48', lts: false},
+  // {runtime: 'node', target: '7.0.0', abi: '51', lts: false},
+  // {runtime: 'node', target: '8.0.0', abi: '57', lts: new Date() < new Date(2019, 4, 31)},
+  // {runtime: 'node', target: '9.0.0', abi: '59', lts: false},
+  // {runtime: 'node', target: '10.0.0', abi: '64', lts: new Date(2018, 10, 1) < new Date() < new Date(2020, 4, 31)},
+  // {runtime: 'electron', target: '0.36.0', abi: '47', lts: false},
+  // {runtime: 'electron', target: '1.1.0', abi: '48', lts: false},
+  // {runtime: 'electron', target: '1.3.0', abi: '49', lts: false},
+  // {runtime: 'electron', target: '1.4.0', abi: '50', lts: false},
+  // {runtime: 'electron', target: '1.5.0', abi: '51', lts: false},
+  // {runtime: 'electron', target: '1.6.0', abi: '53', lts: false},
+  // {runtime: 'electron', target: '1.7.0', abi: '54', lts: false},
+  // {runtime: 'electron', target: '1.8.0', abi: '57', lts: false},
+  // {runtime: 'electron', target: '2.0.0', abi: '57', lts: false},
+  // {runtime: 'electron', target: '3.0.0-beta.1', abi: '64', lts: false},
+  // {runtime: 'electron', target: '3.0.0-beta.10', abi: '64', lts: false},
+  // {runtime: 'electron', target: '3.0.0-beta.11', abi: '64', lts: false},
+  {runtime: 'electron', target: '3.0.0-beta.12', abi: '64', lts: false}
 ]
 
 var additionalTargets = [
-  {runtime: 'node-webkit', target: '0.13.0', abi: '47', lts: false},
-  {runtime: 'node-webkit', target: '0.15.0', abi: '48', lts: false},
-  {runtime: 'node-webkit', target: '0.18.3', abi: '51', lts: false},
-  {runtime: 'node-webkit', target: '0.23.0', abi: '57', lts: false},
-  {runtime: 'node-webkit', target: '0.26.5', abi: '59', lts: false}
+  // {runtime: 'node-webkit', target: '0.13.0', abi: '47', lts: false},
+  // {runtime: 'node-webkit', target: '0.15.0', abi: '48', lts: false},
+  // {runtime: 'node-webkit', target: '0.18.3', abi: '51', lts: false},
+  // {runtime: 'node-webkit', target: '0.23.0', abi: '57', lts: false},
+  // {runtime: 'node-webkit', target: '0.26.5', abi: '59', lts: false}
 ]
 
 var deprecatedTargets = [
-  {runtime: 'node', target: '0.2.0', abi: '1', lts: false},
-  {runtime: 'node', target: '0.9.1', abi: '0x000A', lts: false},
-  {runtime: 'node', target: '0.9.9', abi: '0x000B', lts: false},
-  {runtime: 'node', target: '0.10.4', abi: '11', lts: false},
-  {runtime: 'node', target: '0.11.0', abi: '0x000C', lts: false},
-  {runtime: 'node', target: '0.11.8', abi: '13', lts: false},
-  {runtime: 'node', target: '0.11.11', abi: '14', lts: false},
-  {runtime: 'node', target: '1.0.0', abi: '42', lts: false},
-  {runtime: 'node', target: '1.1.0', abi: '43', lts: false},
-  {runtime: 'node', target: '2.0.0', abi: '44', lts: false},
-  {runtime: 'node', target: '3.0.0', abi: '45', lts: false},
-  {runtime: 'node', target: '4.0.0', abi: '46', lts: false},
-  {runtime: 'electron', target: '0.30.0', abi: '44', lts: false},
-  {runtime: 'electron', target: '0.31.0', abi: '45', lts: false},
-  {runtime: 'electron', target: '0.33.0', abi: '46', lts: false}
+  // {runtime: 'node', target: '0.2.0', abi: '1', lts: false},
+  // {runtime: 'node', target: '0.9.1', abi: '0x000A', lts: false},
+  // {runtime: 'node', target: '0.9.9', abi: '0x000B', lts: false},
+  // {runtime: 'node', target: '0.10.4', abi: '11', lts: false},
+  // {runtime: 'node', target: '0.11.0', abi: '0x000C', lts: false},
+  // {runtime: 'node', target: '0.11.8', abi: '13', lts: false},
+  // {runtime: 'node', target: '0.11.11', abi: '14', lts: false},
+  // {runtime: 'node', target: '1.0.0', abi: '42', lts: false},
+  // {runtime: 'node', target: '1.1.0', abi: '43', lts: false},
+  // {runtime: 'node', target: '2.0.0', abi: '44', lts: false},
+  // {runtime: 'node', target: '3.0.0', abi: '45', lts: false},
+  // {runtime: 'node', target: '4.0.0', abi: '46', lts: false},
+  // {runtime: 'electron', target: '0.30.0', abi: '44', lts: false},
+  // {runtime: 'electron', target: '0.31.0', abi: '45', lts: false},
+  // {runtime: 'electron', target: '0.33.0', abi: '46', lts: false}
 ]
 
 var futureTargets = [
